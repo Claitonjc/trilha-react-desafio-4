bc1bb1092cbab04559c8ecd57f87d3fa75174277
Claitonjc <claitoncharlene@outook.com>
2024-08-01 18:51:39 -0300

    Envio do projeto com o desafio concluido

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -14,4 +14,11 @@ export const ButtonContainer = styled.button`
         opacity: 0.6;
         cursor:pointer;
     }
+
+    &:disabled {
+        background-color: gray;
+        border: none;
+        cursor: auto;
+        opacity: 80%;
+    }
 `
--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -25,8 +25,10 @@ const Login = () => {
     resolver: yupResolver(schema),
     mode: "onBlur",
     defaultValues,
-    reValidateMode: "onChange",
+    reValidateMode: "onChange", 
   });
+
+  console.log(isValid);
 
   return (
     <Container>
@@ -49,7 +51,7 @@ const Login = () => {
             errorMessage={errors?.password?.message}
           />
           <Spacing />
-          <Button title="Entrar" />
+          <Button title="Entrar" disabled={!isValid}/>
         </Column>
       </LoginContainer>
     </Container>
